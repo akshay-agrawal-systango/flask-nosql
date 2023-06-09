eebb441d4488b5a053fb22091d7a8a9241a2ef17
Akshay Agrawal <aagrawal@systango.com>
2023-06-09 13:47:55 +0530

    updates

--- queries.py
+++ queries.py
@@ -2,11 +2,8 @@ from app import db
 
 laptops_ref = db.collection('laptops')
 
-query1 = laptops_ref.where('tags', 'array_contains', '2023')
-query2 = laptops_ref.where('tags', 'array_contains', '2023')
-query1.get()
-query2.get()
-
+laptops_ref.where('tags', 'array_contains', '2023').get()
+laptops_ref.where('tags', 'array_contains', '2023').limit(1).get()
 
 laptops_ref = db.collection('laptops').document('1')
 laptops_ref.update({"tags": ["lenovo", "2023", "ultrabook", "intel"]})
@@ -17,3 +14,13 @@ access1_ref = laptops_ref.collection('accessories').document("1")
 access1_ref.set({"name": "mouse"})
 access2_ref = laptops_ref.collection('accessories').document("1")
 access2_ref.set({"name": "keyboard"})
+
+
+laptops_ref = db.collection('laptops').document("1")
+laptops_ref.get().get("brand")
+
+laptops_ref = db.collection('laptops').where("brand", "==", "HP").get()
+
+
+laptops_ref = db.collection('laptops').document("1")
+laptops_ref.delete()
